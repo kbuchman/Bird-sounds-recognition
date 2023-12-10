e5b051839559592955c585981656ee277c96f95c
Jan Binkowski <jan.binkowski.01@gmail.com>
2023-12-10 19:46:51 +0100

    Add descriptions

--- src/Files download/loader.py
+++ src/Files download/loader.py
@@ -1,6 +1,3 @@
-'''
-A function that downloads, unzips, resamples converts to .wav and loads audio files as np.arrays for further usage
-'''
 import os
 import zipfile
 from gdown import download_folder
@@ -16,13 +13,30 @@ import mutagen
 
 
 def load_files(drive_link: str, zip_name: str):
+    """
+    Function downolads, unzips, resamples, normalizes and converts to .wav database from google drive 
+
+    Args:
+        drive_link (str): Link to google drive folder
+        zip_name (str): Name of zip file form google drive folder
+    """
     dataFolder = download_from_drive_unzip(drive_link, zip_name)
     for file in os.listdir(dataFolder):
-        convert_resample(f"{dataFolder}\\{file}")
+        resample_convert(f"{dataFolder}\\{file}")
     pass
 
 
 def download_from_drive_unzip(drive_link: str, zip_name: str) -> str:
+    """
+    Fucntion downolads a zip file from google drive, creats work folder "DataBase" and extracts files from zip file there
+
+    Args:
+        drive_link (str): Link to google drive folder, viewer premission must be turned on
+        zip_name (str): Name of zip file that is downloaded from drive
+
+    Returns:
+        str: path to "DataBase" folder
+    """
     dataFolder = os.getcwd() + "/DataBase"
     if not os.path.exists(dataFolder):
         os.mkdir(dataFolder)
@@ -33,7 +47,13 @@ def download_from_drive_unzip(drive_link: str, zip_name: str) -> str:
     return dataFolder
 
 
-def convert_resample(file: str):
+def resample_convert(file: str):
+    """
+    Function resamples given audio file, normalizes it and converts to .wav 
+
+    Args:
+        file (str): audio file
+    """
     #TODO
         # ffmpeg does not work on my files, works on previous files from TM
     # check sample rate
@@ -45,6 +65,12 @@ def convert_resample(file: str):
 
 
 def normalize_convert_to_wav(file: str):
+    """
+    Function normalizes audio file and exports it to .wav format
+
+    Args:
+        file (str): audio file
+    """
     rawsound = AudioSegment.from_file(file, file[-3:])  
     normalizedsound = effects.normalize(rawsound)  
     normalizedsound.export(f"{file[:-4]}norm.wav", format="wav")
