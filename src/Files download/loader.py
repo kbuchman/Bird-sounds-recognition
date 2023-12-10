0a511671f03799bb4da13321ec8a4bfd8c12cdf1
Jan Binkowski <jan.binkowski.01@gmail.com>
2023-12-10 15:50:32 +0100

    ffmpeg, conversion, normalization

--- src/Files download/loader.py
+++ src/Files download/loader.py
@@ -4,42 +4,47 @@ A function that downloads, unzips, resamples converts to .wav and loads audio fi
 import os
 import zipfile
 from gdown import download_folder
+from pydub import AudioSegment, effects
+import mutagen
 
 #TODO:
 # for each file:
-    # check extension
-        # convert to .wav
     # check sampling
-        # resample
+        # resample !!!!!
     # check length (what for? dunno, maybe useful)
-    # normalize 
     # save as converted file or load converted file as np.array
 
 
-def load_files():
+def load_files(drive_link: str, zip_name: str):
+    dataFolder = download_from_drive_unzip(drive_link, zip_name)
+    for file in os.listdir(dataFolder):
+        convert_resample(f"{dataFolder}\\{file}")
     pass
 
 
-def download_from_drive(drive_link: str, zip_name: str):
+def download_from_drive_unzip(drive_link: str, zip_name: str) -> str:
     dataFolder = os.getcwd() + "/DataBase"
     if not os.path.exists(dataFolder):
         os.mkdir(dataFolder)
         download_folder(drive_link, quiet=True, use_cookies=False, remaining_ok=True, output=dataFolder)
         with zipfile.ZipFile(dataFolder + zip_name) as zip_file:
             zip_file.extractall(dataFolder)
-    pass
+        os.remove(f"{dataFolder}\\{zip_name}")
+    return dataFolder
 
 
-def unzip_to_workfolder():
-    pass
-
-
-def convert_resample():
+def convert_resample(file: str):
     #TODO
-    # check extension
-    # check saple rate
-    pass
+        # ffmpeg does not work on my files, works on previous files from TM
+    # check sample rate
+        # resample
+    audio_info = mutagen.File(file).info
+    if not audio_info.sample_rate == 16000:
+        print(audio_info.sample_rate)
+    normalize_convert_to_wav(file)
 
 
-def normalize():
-    pass
+def normalize_convert_to_wav(file: str):
+    rawsound = AudioSegment.from_file(file, file[-3:])  
+    normalizedsound = effects.normalize(rawsound)  
+    normalizedsound.export(f"{file[:-4]}norm.wav", format="wav")
