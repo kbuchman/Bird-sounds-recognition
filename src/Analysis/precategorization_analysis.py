d7bb60f6c813a2328b5b27602d01cfe10852b144
JadwigaHyla <hylajadzia@gmail.com>
2023-12-17 19:24:11 +0100

    fix code

--- src/Analysis/precategorization_analysis.py
+++ src/Analysis/precategorization_analysis.py
@@ -5,17 +5,17 @@ def framing(signal, fs, win_len, win_hop):
     frame_step = win_hop * fs 
     signal_length = len(signal)
     frames_overlap = frame_length - frame_step
-    num_frames = np.abs(signal_length - frames_overlap)//np.abs(frame_length - frames_overlap)
+    num_frames = np.abs(signal_length - frames_overlap) // np.abs(frame_length - frames_overlap)
     frame_list = []
     for i in range(num_frames):
-        start = i*(frame_length-frame_step)
-        frame_list.append(signal[start:start+frame_length])
+        start = i * (frame_length - frame_step)
+        frame_list.append(signal[start : start + frame_length])
     return frame_list
 
 def signal_energy(frame_list):
     energy = []
     for frame in frame_list:
-        power = sc.sum(frame*frame,1)/frame.size
+        power = sc.sum(frame * frame, 1) / frame.size
         energy.append(power * frame.size)
     return energy
 
@@ -26,7 +26,7 @@ def main():
     win_hop = 0
     frame_list = []
     energy = []
-    frame_list =framing(signal, fs, win_len, win_hop)
+    frame_list = framing(signal, fs, win_len, win_hop)
     energy = signal_energy(frame_list)
     std = np.stdev(energy)
 
